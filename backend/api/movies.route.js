71c33d04a6c4d5f1bc8b1b7df7db75a766fb294d
hem193 <hemanthcastel@gmail.com>
2023-01-01 22:58:19 +0530

    added Movies controller

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,5 +1,6 @@
 import express from "express";
+import MoviesController from "./movies.controller";
 const router = express.Router(); // get access to express router
 
-router.route("/").get((req, res) => res.send("hello world"));
+router.route("/").get(MoviesController.apiGetMovies);
 export default router;
