987cb977fc7306f92d7cba949426df2577e7b75c
hem193 <hemanthcastel@gmail.com>
2023-01-02 16:55:15 +0530

    added React frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
