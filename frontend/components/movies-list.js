94b0854092ce107ba705b31d1e9a955e3fe0cc63
hem193 <hemanthcastel@gmail.com>
2023-01-02 19:51:35 +0530

    added Navbar

---
+++ frontend/components/movies-list.js
@@ -0,0 +1,5 @@
+function MoviesList() {
+  return <div className="App">MoviesList</div>;
+}
+
+export default MoviesList;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import AddReview from "../components/add-review";
+// import MoviesList from "../components/movies-list";
+// import Movie from "../components/movie";
+// import Login from "../components/login";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to="/movies">Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a onClick={logout}>Logout User</a>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default App;
