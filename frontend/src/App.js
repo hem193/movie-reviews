38c4c3499effad5573b3d49142a6436fa978bb15
hem193 <hemanthcastel@gmail.com>
2023-01-03 14:43:55 +0530

    added adding & editing reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,64 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import AddReview from "./components/add-review";
 import MoviesList from "./components/movies-list";
 import Movie from "./components/movie";
 import Login from "./components/login";
-
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = React.useState(null);
 
   async function login(user = null) {
+    // default user to null
     setUser(user);
   }
+
   async function logout() {
     setUser(null);
   }
+
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/movies">Movies</Link>
-            </Nav.Link>
-            <Nav.Link>
-              {user ? (
-                <button onclick={logout}>Logout User</button>
-              ) : (
-                <Link to="/login">Login</Link>
-              )}
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Container>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>Movie Reviews</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>
+                <Link to={"/movies"}>Movies</Link>
+              </Nav.Link>
+              <Nav.Link>
+                {user ? (
+                  <Link onClick={logout}>Logout User</Link>
+                ) : (
+                  <Link to={"/login"}>Login</Link>
+                )}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-      <Routes>
-        <Route path="/movies" element={<MoviesList />}></Route>
-        <Route
-          path="/movies/:id/review"
-          element={(props) => <AddReview {...props} user={user} />}
-        ></Route>
-        <Route
-          path="/movies/:id/"
-          element={(props) => <Movie {...props} user={user} />}
-        ></Route>
-        <Route
-          path="/login"
-          element={(props) => <Login {...props} login={login} />}
-        ></Route>
-      </Routes>
+        <Switch>
+          <Route exact path={["/", "/movies"]} component={MoviesList}></Route>
+          <Route
+            path="/movies/:id/review"
+            render={(props) => <AddReview {...props} user={user} />}
+          ></Route>
+          <Route
+            path="/movies/:id/"
+            render={(props) => <Movie {...props} user={user} />}
+          ></Route>
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} login={login} />}
+          ></Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
